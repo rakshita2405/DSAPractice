b1392f29d020250722f7576565a80648c6c7db1a
Rakshita Bhardwaj <122474971+rakshita2405@users.noreply.github.com>
2025-09-19 03:03:54 +0530

    Time: 0 ms (100%), Space: 42 MB (19.29%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,13 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] words = s.trim().split(" ");
-        Stack<String> st = new Stack<>();
-        
-        for (String word : words) {
-            st.push(word);
-        }
-        
-        String last = st.pop();  // get last word
-        return last.length();
+         String[] str = s.trim().split(" "); // trim() removes leading and trailing spaces
+        return str[str.length - 1].length();
     }
 }
