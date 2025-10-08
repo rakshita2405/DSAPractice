04b6589bb198f9ac729c87ced7bbd022c2fafdf5
Rakshita Bhardwaj <122474971+rakshita2405@users.noreply.github.com>
2025-10-08 23:55:36 +0530

    Time: 0 ms (100%), Space: 40.8 MB (64.56%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,18 @@
+class Solution {
+     public static boolean isHappy(int n) {
+        while (n != 1 && n != 4) {
+            n = getSumOfSquares(n);
+        }
+        return n == 1;
+    }
+
+    private static int getSumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}
